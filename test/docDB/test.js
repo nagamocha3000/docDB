cce56350aa5c22191d836c7d4f37436dbc00b929
nagamocha3000 <nagamocha@gmail.com>
2020-01-17 16:27:43 +0300

    add pending test for basic db level methods

---
+++ test/docDB/test.js
@@ -0,0 +1,14 @@
+const assert = require("assert");
+const docDB = require("../../src/docDB");
+
+describe("docDB", function() {
+    before(function() {});
+    after(function() {});
+
+    it("should load without throwing an error");
+    it("should create new database");
+    it("should connect to a db that's already been created");
+    it("should throw error if one tries to create a db that already exists");
+    it("should delete database");
+    it("should clear a database");
+});
