de29d6a225bbde9a43a6aec5ce60cdced6c10f4f
nagamocha3000 <nagamocha@gmail.com>
2020-01-17 23:57:13 +0300

    add test to check if one attempts to connect to a db that doesn't exist

--- test/docDB/test.js
+++ test/docDB/test.js
@@ -1,14 +1,45 @@
 const assert = require("assert");
-const docDB = require("../../src/docDB");
+const expect = require("chai").expect;
 
 describe("docDB", function() {
-    before(function() {});
-    after(function() {});
+    it("should load module without throwing an error", () => {
+        const loadDocDBModule = function() {
+            let _ = require("../../src/docDB");
+        };
+        expect(loadDocDBModule).to.not.throw();
+    });
 
-    it("should load without throwing an error");
-    it("should create new database");
-    it("should connect to a db that's already been created");
-    it("should throw error if one tries to create a db that already exists");
-    it("should delete database");
-    it("should clear a database");
+    describe("in general", function() {
+        let docDB,
+            dbName = "TEST";
+
+        before(function() {
+            docDB = require("../../src/docDB");
+        });
+
+        it("should throw an error if one attempts to connect to a database that does not exist", function() {
+            expect(docDB.connect).to.be.a("function");
+            expect(() => {
+                docDB.connect(dbName);
+            }).to.throw("db does not exist");
+        });
+
+        it("should be able to create a new database");
+
+        it(
+            "should throw an error if one attempts to create a database that already exists"
+        );
+
+        it(
+            "should be able to connect to a database that's already been created"
+        );
+
+        it("should delete a database");
+
+        // it("should create new database", function() {
+        //     let db = docDB.createDB(dbName);
+        //     expect(db).to.not.be.undefined;
+        //     expect(db).to.not.be.null;
+        // });
+    });
 });
