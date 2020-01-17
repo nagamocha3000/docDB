6af1153b552ad6d7432ad4700f21152323ede095
nagamocha3000 <nagamocha@gmail.com>
2020-01-17 23:48:30 +0300

    remove conn file, general db methods to be added in a different file

--- src/docDB/conn.js
+++
@@ -1,8 +0,0 @@
-const level = require("level");
-const path = require("path");
-const dbPath = path.resolve(__dirname, "./_db");
-
-module.exports.connect = (dbName, opts) => {
-    opts = opts || { valueEncoding: "json" };
-    return level(path.resolve(dbPath, dbName), opts);
-};
