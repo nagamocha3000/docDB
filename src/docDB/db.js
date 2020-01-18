5bac65b9de039ccdd9daa67f78cbfa3203facb34
nagamocha3000 <nagamocha@gmail.com>
2020-01-18 12:46:35 +0300

    add createDB, deleteDB, connect, exists functions

--- src/docDB/db.js
+++ src/docDB/db.js
@@ -2,19 +2,46 @@ const level = require("level");
 const path = require("path");
 const dbPath = path.resolve(__dirname, "./_db");
 const fs = require("fs");
+const fse = require("fs-extra");
 
-const getDBs = () => {
-    let contents = fs.readdirSync(dbPath, { withFileTypes: true });
-    let dbNames = contents
-        .filter(entry => entry.isDirectory())
-        .map(entry => entry.name);
-    return dbNames;
+const getDB = (() => {
+    let openedDBs = new Map();
+    let opts = { valueEncoding: "json" };
+    return dbName => {
+        let db = openedDBs.get(dbName);
+        if (db === undefined) {
+            try {
+                db = level(path.resolve(dbPath, dbName), opts);
+                openedDBs.set(dbName, db);
+            } catch (error) {
+                console.error("err2");
+            }
+        }
+        return db;
+    };
+})();
+
+const createDB = dbName => {
+    if (exists(dbName)) throw new Error("db already exists");
+    return getDB(dbName);
 };
 
 const connect = dbName => {
-    throw new Error("db does not exist");
+    if (exists(dbName)) return getDB(dbName);
+    else throw new Error("db does not exist");
 };
 
-const exists = dbName => getDBs().includes(dbName);
+const exists = dbName =>
+    fs
+        .readdirSync(dbPath, { withFileTypes: true })
+        .filter(entry => entry.isDirectory())
+        .map(entry => entry.name)
+        .includes(dbName);
 
-module.exports = { connect, exists };
+const deleteDB = dbName => {
+    if (exists(dbName)) {
+        fse.removeSync(path.resolve(dbPath, dbName));
+    } else throw new Error("db does not exist");
+};
+
+module.exports = { connect, exists, createDB, deleteDB };
